613814d7278cdd8676323991c05215113ceed9ab
Adrian <adrian_zinko@op.pl>
2021-03-07 14:05:46 +0100

    Manage monitor class

--- src/main/java/com/practice/App.java
+++ src/main/java/com/practice/App.java
@@ -2,6 +2,10 @@ package com.practice;
 
 public class App {
     public static void main(String[] args) {
+        Monitor monitor = new Monitor();
+        System.out.println(monitor.getResolution());
 
+        monitor.setLowResolution();
+        System.out.println(monitor.getResolution());
     }
 }
--- src/main/java/com/practice/Headphones.java
+++ src/main/java/com/practice/Headphones.java
@@ -1,4 +1,13 @@
 package com.practice;
 
 public class Headphones {
+    private final String name;
+
+    public Headphones(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
