79007e280835ed9e82c64066197b8f6bf26b5a18
Adrian <adrian_zinko@op.pl>
2021-03-07 19:58:31 +0100

    Execute objects

--- src/main/java/com/practice/App.java
+++ src/main/java/com/practice/App.java
@@ -1,11 +1,30 @@
 package com.practice;
 
+import com.practice.drive.HDDDrive;
+import com.practice.drive.SSDDrive;
+
+import java.util.Arrays;
+
 public class App {
     public static void main(String[] args) {
         Monitor monitor = new Monitor();
-        System.out.println(monitor.getResolution());
+        SSDDrive ssdDrive = new SSDDrive();
+        HDDDrive hddDrive = new HDDDrive();
 
-        monitor.setLowResolution();
-        System.out.println(monitor.getResolution());
+        Computer computer = new Computer(monitor, hddDrive);
+
+        for (String s1 : Arrays.asList("japko1.jpg", "korona1.jpg", "slub1.jpg")) {
+            ssdDrive.addFile(new File(s1));
+        }
+        for (String s : Arrays.asList("chorwacja1.jpg", "kosc1.jpg")) {
+            hddDrive.addFile(new File(s));
+        }
+
+        System.out.println("SSD Files: ");
+        ssdDrive.displayFiles();
+        System.out.println();
+        System.out.println("HDD Files: ");
+        hddDrive.displayFiles();
+
     }
 }
