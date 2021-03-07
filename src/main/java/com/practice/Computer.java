9d39da902777bbc8067c008cc0e0e7c9ea2cc032
Adrian <adrian_zinko@op.pl>
2021-03-07 13:56:53 +0100

    Set getters and setters

--- src/main/java/com/practice/Computer.java
+++ src/main/java/com/practice/Computer.java
@@ -9,4 +9,37 @@ public class Computer {
     private Headphones headphones;
 
     List<USBDevice> usbDevices = new ArrayList<>();
+
+    public Computer(Monitor monitor, Drive drive) {
+        this.monitor = monitor;
+        this.drive = drive;
+    }
+
+    public Monitor getMonitor() {
+        return monitor;
+    }
+
+    public void setMonitor(Monitor monitor) {
+        this.monitor = monitor;
+    }
+
+    public Drive getDrive() {
+        return drive;
+    }
+
+    public void setDrive(Drive drive) {
+        this.drive = drive;
+    }
+
+    public Headphones getHeadphones() {
+        return headphones;
+    }
+
+    public void setHeadphones(Headphones headphones) {
+        this.headphones = headphones;
+    }
+
+    public List<USBDevice> getUsbDevices() {
+        return usbDevices;
+    }
 }
