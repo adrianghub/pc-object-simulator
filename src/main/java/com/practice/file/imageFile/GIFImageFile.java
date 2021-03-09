bbf76a9b9fa6d8eff7dae78538fa0bfbe4105bcf
Adrian <adrian_zinko@op.pl>
2021-03-09 07:43:56 +0100

    Add repeated code to abstract class

--- src/main/java/com/practice/file/imageFile/GIFImageFile.java
+++ src/main/java/com/practice/file/imageFile/GIFImageFile.java
@@ -1,32 +1,10 @@
 package com.practice.file.imageFile;
 
-import com.practice.file.File;
-import com.practice.file.FileType;
+public class GIFImageFile extends AbstractImageFile {
 
-public class GIFImageFile implements File {
-    private String name;
-    private int size;
-    private final FileType fileType;
 
-    public GIFImageFile(String name, int size, FileType fileType) {
-        this.name = name;
-        this.size = size;
-        this.fileType = fileType;
-    }
-
-    @Override
-    public String getName() {
-        return this.name;
-    }
-
-    @Override
-    public int getSize() {
-        return this.size;
-    }
-
-    @Override
-    public FileType getType() {
-        return null;
+    public GIFImageFile(String name, int size) {
+        super(name, size);
     }
 
     public void displayAnimation() {
--- src/main/java/com/practice/file/imageFile/JPGImageFile.java
+++ src/main/java/com/practice/file/imageFile/JPGImageFile.java
@@ -1,34 +1,11 @@
 package com.practice.file.imageFile;
 
-import com.practice.file.File;
-import com.practice.file.FileType;
-
-public class JPGImageFile implements File {
-    private final FileType fileType;
-    private String name;
-    private int size;
+public class JPGImageFile extends AbstractImageFile {
     private int compression;
 
-    public JPGImageFile(String name, int size, FileType fileType, int compression) {
-        this.fileType = fileType;
+    public JPGImageFile(String name, int size, int compression) {
+        super(name, size);
         this.compression = compression;
-        this.name = name;
-        this.size = size;
-    }
-
-    @Override
-    public String getName() {
-        return this.name;
-    }
-
-    @Override
-    public int getSize() {
-        return this.size;
-    }
-
-    @Override
-    public FileType getType() {
-        return this.fileType;
     }
 
     public int getCompression() {
